681eeeea0c196ce2c627e771adbb3ab4bd6af652
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-10-16 11:39:31 +0900

    Uncomment detect blueprint registration

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from scoring import scoring_bp
-# from detect import detect_bp  楞谷担当
+from detect import detect_bp
 
 def create_app():
     app = Flask(__name__, static_folder="static")
 
     # Blueprint 登録
     app.register_blueprint(scoring_bp)
-    # app.register_blueprint(detect_bp)  楞谷担当
+    app.register_blueprint(detect_bp)
 
     # トップページ
     @app.route('/')
@@ -21,4 +21,5 @@ if __name__ == '__main__':
     app = create_app()
     print("アプリ起動中: http://127.0.0.1:5000/")
     app.run(debug=True)
+
 
