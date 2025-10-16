a1aa7733f0b797e1e2dcd7a8366ad11c6ef23193
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-10-16 11:09:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from scoring import scoring_bp
-from detect import detect_bp
+# from detect import detect_bp  楞谷担当
 
 def create_app():
     app = Flask(__name__, static_folder="static")
 
     # Blueprint 登録
     app.register_blueprint(scoring_bp)
-    app.register_blueprint(detect_bp)
+    # app.register_blueprint(detect_bp)  楞谷担当
 
     # トップページ
     @app.route('/')
@@ -21,3 +21,4 @@ if __name__ == '__main__':
     app = create_app()
     print("アプリ起動中: http://127.0.0.1:5000/")
     app.run(debug=True)
+
