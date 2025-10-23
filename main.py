c540c7ab831eb0038ca1652be0843de09262f5fe
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-10-23 09:41:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from flask import Flask, render_template
-from scoring import scoring_bp
-from detect import detect_bp
 
 app = Flask(__name__, static_folder="static")
 
-# Blueprint 登録
-app.register_blueprint(scoring_bp)
-app.register_blueprint(detect_bp)
-
-# トップページ
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 if __name__ == '__main__':
     print("アプリ起動中: http://127.0.0.1:5000/")
     app.run(debug=True)
-
