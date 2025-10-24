b3115988b520d704fce1fd205a6e0b39942938f9
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-10-24 09:30:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_folder="static", template_folder=os.path.abspath("."))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    print("アプリ起動中: http://127.0.0.1:5000/")
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
