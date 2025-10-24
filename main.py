4c893b33ddd302aca39ce42c937dcf31c59268f6
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-10-24 09:59:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
-import os
 
-app = Flask(__name__, static_folder="static", template_folder=os.path.abspath("."))
+app = Flask(__name__, template_folder='scoring/templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # プロジェクト直下の index.html を表示
+    return render_template('../index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route('/scoring')
+def scoring():
+    # scoring/templates/saiten.html を表示
+    return render_template('saiten.html')
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
